b048dc3515ecc8ed62b46a4f484319f2ffcbd412
Mateus <mateustpinheiro@gmail.com>
2026-06-29 22:32:52 -0300

    Adição da entidade de categoria

---
+++ src/main/java/agroFiscal/model/Categoria.java
@@ -0,0 +1,22 @@
+package agroFiscal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nome;
+    
+}
