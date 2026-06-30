600b9d7fd420a2655ffccd6d9c489424daedac5d
Mateus <mateustpinheiro@gmail.com>
2026-06-29 22:56:18 -0300

    Adição da classe de serviço de categoria

---
+++ src/main/java/agroFiscal/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package agroFiscal.service;
+
+import agroFiscal.model.Categoria;
+import agroFiscal.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria salvar(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+}
