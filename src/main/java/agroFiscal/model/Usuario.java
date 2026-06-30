be4f12b2080a38395b358405b115feaf529afc54
Mateus <mateustpinheiro@gmail.com>
2026-06-29 22:38:21 -0300

    Adição da entidade de usuário

---
+++ src/main/java/agroFiscal/model/Usuario.java
@@ -0,0 +1,31 @@
+package agroFiscal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, length = 30)
+    private String tipoPerfil;
+
+}
