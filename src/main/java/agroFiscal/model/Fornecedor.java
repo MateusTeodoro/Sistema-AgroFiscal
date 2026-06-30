7734a4e0c268ad28e2f0c7eac2b6995991c85413
Mateus <mateustpinheiro@gmail.com>
2026-06-29 22:20:09 -0300

    Adição da entidade Fornecedor

---
+++ src/main/java/agroFiscal/model/Fornecedor.java
@@ -0,0 +1,29 @@
+package agroFiscal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_fornecedor")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 18)
+    private String cnpj;
+
+    @Column(nullable = false, length = 150)
+    private String razaoSocial;
+
+    @Column(length = 20)
+    private String telefone;
+}
